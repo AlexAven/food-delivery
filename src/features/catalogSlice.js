7257d50ac4db693fd6428be7694e4341f873d411
Alexander Kazakov <gloom31@gmail.com>
2024-11-20 19:20:46 +0300

    slice refactoring

--- src/features/catalogSlice.js
+++ src/features/catalogSlice.js
@@ -4,12 +4,14 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 const initialState = {
   entities: {},
   ids: [],
+  status: null,
 };
 
-export const getCatalog = createAsyncThunk('@@catalog/get-catalog', async (_, { dispatch }) => {
+export const getCatalog = createAsyncThunk('@@catalog/get-catalog', async () => {
   const res = await axios.get('http://localhost:3001/entities');
   const data = res.data;
-  dispatch(createCatalog(data));
+
+  return data;
 });
 
 const catalogSlice = createSlice({
@@ -25,6 +27,25 @@ const catalogSlice = createSlice({
         }
       });
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCatalog.pending, (state) => {
+        state.status = 'Loading...';
+      })
+      .addCase(getCatalog.rejected, (state) => {
+        state.status = "Couldn't fetch any data";
+      })
+      .addCase(getCatalog.fulfilled, (state, { payload }) => {
+        payload.forEach((item) => {
+          const itemId = item.id;
+          if (!state.entities[itemId]) {
+            state.entities[itemId] = item;
+            state.ids.push(itemId);
+            state.error = null;
+          }
+        });
+      });
   },
 });
 
